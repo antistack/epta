93205ec1da882c6aa55e8509994e08227cb08863
antistack <f3yrsnn@gmail.com>
2022-04-02 17:36:21 +0300

    save_crops now common for any amount of levels inside

--- epta/utils/utils.py
+++ epta/utils/utils.py
@@ -1,18 +1,22 @@
 import cv2 as cv
 import os
+from typing import Union
 
-
-def save_crops(crops: dict, save_path: str, prefix=''):
+def _save_crops(crops: Union[dict, 'np.ndarray'], save_path: str, prefix=''):
     # single leveled dictionary
-    for name, crop in crops.items():
-        if crop is not None:
-            save_name = os.path.join(save_path, f'{prefix}_{name}.png')
-            crop = cv.cvtColor(crop, cv.COLOR_RGB2BGR)
+    if isinstance(crops, dict):
+        for name, crop in crops.items():
+            _save_crops(crop, save_path, prefix=f'{prefix}_{name}')
+    else:
+        if crops is not None:
+            if prefix.startswith('_'):
+                prefix = prefix[1:]
+            save_name = os.path.join(save_path, f'{prefix}.png')
+            crop = cv.cvtColor(crops, cv.COLOR_RGB2BGR)
             cv.imwrite(save_name, crop)
 
-def save_2d_crops(crops: dict, save_path: str):
+def save_crops(crops: dict, save_path: str):
     # 2 leveled dictionary
     if not os.path.exists(temp := os.path.dirname(save_path)):
         os.makedirs(temp)
-    for cropper_name in crops:
-        save_crops(crops[cropper_name], save_path, prefix=cropper_name)
+    _save_crops(crops, save_path)
